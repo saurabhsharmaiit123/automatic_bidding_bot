65d81baecf15733ac9714fb84a1484fd2c597a65
Saurabh Sharma <52253058+saurabhsharmaiit123@users.noreply.github.com>
2022-07-07 14:39:26 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -3,7 +3,7 @@ from freelancersdk.resources.users \
     import get_self_user_id
 import os
 
-oauth_token='y4OwAnq4GRgArzK2YLdakB1t0kuN5y'
+oauth_token=''   //Keep Hidden
 url='https://freelancer.com'
 session = Session(oauth_token=oauth_token, url=url)
 my_user_id = get_self_user_id(session)
